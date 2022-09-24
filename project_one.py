35bb17413a52ba5d1fceed1efa51fc7008102039
akwarkoh <angelakwarko@gmail.com>
2022-09-24 16:32:33 -0700

    prompting user for input

---
+++ project_one.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+def main():
+
+    input("===Place your Order=== \n a. Fried rice with chicken \n b. Banku and Okro stew \n c.Peanut Butter soup with Rice balls \n d. Yam and Spinach Stew \n \n Pick Your Favorite")
+    
+    menu = input("===Place your Order=== \n a. Fried rice with chicken \n b. Banku and Okro stew \n c.Peanut Butter soup with Rice balls \n d. Yam and Spinach Stew \n \n Pick Your Favorite")
+
+
+
+
+
+
+
+
+
+
+
+
+
+    main()
