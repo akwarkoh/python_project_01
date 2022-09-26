87aaa1d8061fac82fea791580e4fb29b5d38072c
akwarkoh <angelakwarko@gmail.com>
2022-09-25 17:17:32 -0700

    final changes

--- project_one.py
+++ project_one.py
@@ -2,56 +2,63 @@
 
 def main():
 
-    #while True:
+    order = 1
+
+    while order == 1:
     
-    #Prompt user for an input, store user input in a variable and convert user input into an int
-    menu = int(input("===Place your Order=== \n 1. Fried rice with chicken \n 2. Banku and Okro stew \n 3. Peanut Butter soup with Rice balls \n 4. Yam and Spinach Stew \n \n Pick Your Favorite: "))
-     
+        #Prompt user for an input, store user input in a variable and convert user input into an int
+        menu = int(input("===Place your Order=== \n 1. Fried rice with chicken \n 2. Banku and Okro stew \n 3. Peanut Butter soup with Rice balls \n 4. Yam and Spinach Stew \n \n Pick Your Favorite: "))
+        
 
 
-    if menu == 1:
-        print("\n Fried rice with chicken: $15")
-       
-    #prompt user to confirm selection and store in a variable
-        confirm1 = int(input("\n Press 1 to confirm: "))
+        if menu == 1:
+            print("\n Fried rice with chicken: $15")
+        
+        #prompt user to confirm selection and store in a variable
+            confirm1 = int(input("\n Press 1 to confirm: "))
 
-        if confirm1 == 1:
-            print("\n Thanks for placing your order")
+            if confirm1 == 1:
+                print("\n Thanks for placing your order")
 
-    #prompt user to choose a different option and covert user input into an int
+        #prompt user to choose a different option and covert user input into an int
+            else:
+                int(print("\n Choose another option: "))
+                
+        elif menu == 2:
+            print("\n Banku and Okro stew: $17 ")
+            confirm2 = int(input("\n Press 2 to confirm: "))
+        #prompt user to confirm selection and store in a variable
+            if confirm2 == 2:
+                print("\n Thanks for placing your order")
+            else:
+                int(input("\n Choose another option: "))
+
+
+        elif menu == 3:
+            print("\n Peanut Butter soup with Rice balls: $14 ") 
+            confirm3 = int(input("\n Press 3 to confirm: ")) 
+
+            if confirm3 == 3:
+                print("\n Thanks for placing your order")
+            else:
+                int(input("\n Choose another option: "))
+
+
+        elif menu == 4:
+            print("\n Yam and Spinach Stew: $15")
+            confirm4 = int(input("\n Press 4 to confirm: "))
+
+            if confirm4 == 4:
+                print("\n Thanks for placing your order!")
+            else:
+                int(input("\n Choose another option: "))
+
         else:
-            int(print("\n Choose another option: "))
-             
-    elif menu == 2:
-        print("\n Banku and Okro stew: $17 ")
-        confirm2 = int(input("\n Press 2 to confirm: "))
+            print("\n Choose from the provided menu")
 
-        if confirm2 == 2:
-            print("\n Thanks for placing your order")
-        else:
-            int(input("\n Choose another option: "))
+        confirm5 = int(input("\n Press 0 to complete your order and press 1 to continue: "))
+        order = confirm5
 
-
-    elif menu == 3:
-        print("\n 3.Peanut Butter soup with Rice balls: $14 ") 
-        confirm3 = int(input("\Press 3 to confirm: ")) 
-
-        if confirm3 == 3:
-            print("\n Thanks for placing your order")
-        else:
-            int(input("\n Choose another option: "))
-
-
-    elif menu == 4:
-        print("\n Yam and Spinach Stew: $15")
-        confirm4 = int(input("\Press 4 to confirm: "))
-
-        if confirm4 == 4:
-            print("\n Thanks for placing your order")
-        else:
-            int(input("\n Choose another option: "))
-
-    else:
-        print("\n Choose from the provided menu")
+    print("\n Your order will be ready in 15 minutes")   
 
 main()
